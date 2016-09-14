1f9825903aa527e03c45d401fd7930be79a276a3
Zero-Sennin <zerosennin@gmail.com>
2016-09-14 17:52:08 -0400

    Final Move Token Script

--- Match3/Assets/Scripts/MoveTokensScript.cs
+++ Match3/Assets/Scripts/MoveTokensScript.cs
@@ -96,29 +96,50 @@ public class MoveTokensScript : MonoBehaviour
         //		Vector3 movePos1 = Vector3.Lerp(startPos, endPos, lerpPercent);
         //		Vector3 movePos2 = Vector3.Lerp(endPos, startPos, lerpPercent);
 
+
+        /*
+        The next four lines are used to move the tokens.
+        movePos1 is used to move token1 from startPos to endPos.
+        movePos2 is used to move token2 from endPos to startPos.
+        lerpPercent is used to tell the game how far the movement animation has progressed.
+
+        The actual movement is done by setting exchangeToken1's position to movePos1,
+            and by setting exchangeToken2's position to movePos2.
+        */
+
         Vector3 movePos1 = SmoothLerp(startPos, endPos, lerpPercent);
         Vector3 movePos2 = SmoothLerp(endPos, startPos, lerpPercent);
 
         exchangeToken1.transform.position = movePos1;
         exchangeToken2.transform.position = movePos2;
 
-        if (lerpPercent == 1)
+        if (lerpPercent == 1) //When the movement animation is complete:
         {
+            /*
+            The next two lines simply swap exchangeToken1 and exchangeToken2's positions in the grid array of the gameManager script.
+            */
             gameManager.gridArray[(int)exchangeGridPos2.x, (int)exchangeGridPos2.y] = exchangeToken1;
             gameManager.gridArray[(int)exchangeGridPos1.x, (int)exchangeGridPos1.y] = exchangeToken2;
 
-            if (!matchManager.GridHasMatch() && userSwap)
+            if (!matchManager.GridHasMatch() && userSwap) //If there's no match and the user tokens have been swapped:
             {
-                SetupTokenExchange(exchangeToken1, exchangeGridPos2, exchangeToken2, exchangeGridPos1, false);
+                SetupTokenExchange(exchangeToken1, exchangeGridPos2, exchangeToken2, exchangeGridPos1, false); //...reverse the swap.
             }
-            else {
-                exchangeToken1 = null;
-                exchangeToken2 = null;
-                move = false;
+            else { // Otherwise:
+                exchangeToken1 = null; //Make exchangeToken1 empty.
+                exchangeToken2 = null; //Make exchangeToken2 empty.
+                move = false; //We're not moving anymore!
             }
         }
     }
 
+    /// <summary>
+    /// Allows a smooth linear interpolation (lerp) between two positions.
+    /// </summary>
+    /// <param name="startPos">Starting position to lerp from.</param>
+    /// <param name="endPos">Ending position to lerp to.</param>
+    /// <param name="lerpPercent">How far in the lerp (as a percentage) we need to reach.</param>
+    /// <returns>Returns the position that matches the percentage that the object has moved.</returns>
     private Vector3 SmoothLerp(Vector3 startPos, Vector3 endPos, float lerpPercent)
     {
         return new Vector3(
@@ -127,11 +148,25 @@ public class MoveTokensScript : MonoBehaviour
             Mathf.SmoothStep(startPos.z, endPos.z, lerpPercent));
     }
 
+    /// <summary>
+    /// This function is used to animate tokens moving into empty spaces after making a match.
+    /// </summary>
+    /// <param name="startGridX"></param>
+    /// <param name="startGridY"></param>
+    /// <param name="endGridX"></param>
+    /// <param name="endGridY"></param>
+    /// <param name="token"></param>
     public virtual void MoveTokenToEmptyPos(int startGridX, int startGridY,
                                     int endGridX, int endGridY,
                                     GameObject token)
     {
+        /*
+        The next two lines set references to the locations we're using to move the tokens.
+        startPos is set to the place where a token is coming from
+        endPos is set to the empty space's location
 
+        The positions are converted into grid positions by gameManager.GetWorldPositionFromGridPosition.
+        */
         Vector3 startPos = gameManager.GetWorldPositionFromGridPosition(startGridX, startGridY);
         Vector3 endPos = gameManager.GetWorldPositionFromGridPosition(endGridX, endGridY);
 
@@ -139,41 +174,48 @@ public class MoveTokensScript : MonoBehaviour
 
         token.transform.position = pos;
 
-        if (lerpPercent == 1)
+        if (lerpPercent == 1) //If the lerp percentage is complete
         {
-            gameManager.gridArray[endGridX, endGridY] = token;
+            /*
+            Swap the valid token with the empty space.
+            */
+            gameManager.gridArray[endGridX, endGridY] = token; 
             gameManager.gridArray[startGridX, startGridY] = null;
         }
     }
 
+    /// <summary>
+    /// Moves tokens down into the empty spaces after making a match.
+    /// </summary>
+    /// <returns>If tokens have been moved successfully.</returns>
     public virtual bool MoveTokensToFillEmptySpaces()
     {
-        bool movedToken = false;
+        bool movedToken = false; //The variable we'll be using to confirm if a token was moved.
 
-        for (int x = 0; x < gameManager.gridWidth; x++)
+        for (int x = 0; x < gameManager.gridWidth; x++) //For the length of the grid...
         {
-            for (int y = 1; y < gameManager.gridHeight; y++)
+            for (int y = 1; y < gameManager.gridHeight; y++) //...and for the height of the grid...
             {
-                if (gameManager.gridArray[x, y - 1] == null)
+                if (gameManager.gridArray[x, y - 1] == null) //If we find an empty space:
                 {
-                    for (int pos = y; pos < gameManager.gridHeight; pos++)
+                    for (int pos = y; pos < gameManager.gridHeight; pos++) //...using the y position as a reference...
                     {
-                        GameObject token = gameManager.gridArray[x, pos];
-                        if (token != null)
+                        GameObject token = gameManager.gridArray[x, pos]; //...grab a token on top of the empty space.
+                        if (token != null) //If the token that we've just selected is a valid token...
                         {
-                            MoveTokenToEmptyPos(x, pos, x, pos - 1, token);
-                            movedToken = true;
+                            MoveTokenToEmptyPos(x, pos, x, pos - 1, token); //make it move downward.
+                            movedToken = true; //Tell the game that we've moved a token.
                         }
                     }
                 }
             }
         }
 
-        if (lerpPercent == 1)
+        if (lerpPercent == 1) //If the lerp percent is 
         {
             move = false;
         }
 
-        return movedToken;
+        return movedToken; //Tell the game if we moved the object or not.
     }
 }
