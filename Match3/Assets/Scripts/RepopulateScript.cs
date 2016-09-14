6c39d51c48b006b502603f07e27ab3cb3fd586ac
tsarachan <tes353@nyu.edu>
2016-09-14 17:23:53 -0400

    Comments for MatchManager & RepopulateScript
    Added summaries and, where helpful, line-by-line comments.

--- Match3/Assets/Scripts/RepopulateScript.cs
+++ Match3/Assets/Scripts/RepopulateScript.cs
@@ -9,7 +9,13 @@ public class RepopulateScript : MonoBehaviour {
 		gameManager = GetComponent<GameManagerScript>();
 	}
 
+	/// <summary>
+	/// Add tokens at the top of the grid.
+	/// </summary>
 	public virtual void AddNewTokensToRepopulateGrid(){
+
+		//iterate across the top row of the grid
+		//add a new token in all empty spaces
 		for(int x = 0; x < gameManager.gridWidth; x++){
 			GameObject token = gameManager.gridArray[x, gameManager.gridHeight - 1];
 			if(token == null){
