d39c96bf8ae731b76adc1f1f5bea4411e312f5be
dannynanni <dannynanni.us@gmail.com>
2016-09-27 21:07:14 -0400

    SUPER UNCOMFORTABLE

---
+++ Match3MOD/Assets/Scripts/CameraColor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraColor : MonoBehaviour {
+
+		public Color color1 = Color.red;
+		public Color color2 = Color.blue;
+		public float duration = 2.0F;
+
+		Camera camera;
+
+		void Start() {
+			camera = GetComponent<Camera>();
+			camera.clearFlags = CameraClearFlags.SolidColor;
+		}
+
+		void Update() {
+			float t = Mathf.PingPong(Time.time, duration) / duration;
+			camera.backgroundColor = Color.Lerp(color1, color2, t);
+		}
+	}
---
+++ Match3MOD/Assets/Scripts/TokenColor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TokenColor : MonoBehaviour {
+
+	public Color color1 = Color.red;
+	public Color color2 = Color.blue;
+	public float duration = 2.0F;
+
+	SpriteRenderer spriteRenderer;
+
+	void Start() {
+
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	void Update() {
+		float t = Mathf.PingPong(Time.time, duration) / duration;
+		spriteRenderer.color = Color.Lerp(color1, color2, t);
+	}
+}
